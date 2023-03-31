45266be67cc18a0296d9387765d4735e2afbf6db
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-03-30 19:58:15 -0500

    added target element and score ui foundation, working on script

---
+++ BrowserAimTrainer/scripts.js
@@ -0,0 +1,10 @@
+const centerDot = document.getElementsByClassName('.centerDot');
+const middleRing = document.getElementsByClassName('.middleRing');
+const outerRing = document.getElementsByClassName('.outerRing');
+const targetArea = document.getElementsByClassName('.targetArea')''
+
+const onClick = (event) => {
+
+}
+
+document.addEventListener('click', onClick(e));
