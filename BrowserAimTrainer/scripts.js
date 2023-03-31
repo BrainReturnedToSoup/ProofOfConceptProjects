c59008416334d75dc67cd8317261d755cbe32682
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-03-31 04:01:34 -0500

    beginning to reformat the script file, looking for things that can be improved within the script to make it resource intensive, easier to read, etc.

--- BrowserAimTrainer/scripts.js
+++ BrowserAimTrainer/scripts.js
@@ -12,6 +12,9 @@ const HTML = document.getElementsByTagName('html')
 
 let accuracyValues = [];
 let scoreValue = 0
+let timeLeft = 30;
+let onClick;
+let intervalFunction;
 
 const accuracyCalc = (accuracy) => {
     if(timeLeft === 0) return;
@@ -33,9 +36,32 @@ const scoreCalc = () => {
     }
 }
 
-let timeLeft = 30;
-let onClick;
-let intervalFunction;
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+}
+
+const minusOneSecond = () => {
+    timerBox.innerText = `Timer: ${timeLeft} seconds left`;
+    if(timeLeft === 0) {
+        clearInterval(intervalFunction);
+        return gameOver();
+    }
+    timeLeft--;
+}
+
+const gameOver = () => {
+    targetContainer.style.top = "0px";
+    targetContainer.style.left = "0px";
+    storedTarget = targetContainer.children[0];
+    targetContainer.children[0].remove();
+        title.innerText = 'Refresh to try again!';
+}
+
+const moveTheTarget = () => {
+    if(timeLeft === 0) return;
+    targetContainer.style.top = getRandomInt(325) + 'px';
+    targetContainer.style.left = getRandomInt(700) + 'px';
+}
 
 const onStart = (event) => {
     if(onClick !== undefined) { 
@@ -71,35 +97,6 @@ const onStart = (event) => {
     })
     intervalFunction = setInterval(minusOneSecond, 1000);
 }  
-
-
-const minusOneSecond = () => {
-    timerBox.innerText = `Timer: ${timeLeft} seconds left`;
-    if(timeLeft === 0) {
-        clearInterval(intervalFunction);
-        return gameOver();
-    }
-    timeLeft--;
-}
-
-const gameOver = () => {
-    targetContainer.style.top = "0px";
-    targetContainer.style.left = "0px";
-    storedTarget = targetContainer.children[0];
-    targetContainer.children[0].remove();
-        title.innerText = 'Refresh to try again!';
-}
-
-const getRandomInt = (max) => {
-    return Math.floor(Math.random() * max);
-}
-
-
-const moveTheTarget = () => {
-    if(timeLeft === 0) return;
-    targetContainer.style.top = getRandomInt(325) + 'px';
-    targetContainer.style.left = getRandomInt(700) + 'px';
-}
 
 document.addEventListener('click', (e) => {
     onStart(e); 
