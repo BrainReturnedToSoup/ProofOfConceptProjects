9d17b877f3de6f597df67404e58981c458439ec8
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-03-31 04:26:10 -0500

    added some comments inside the script file, about halfway there though

--- BrowserAimTrainer/scripts.js
+++ BrowserAimTrainer/scripts.js
@@ -8,13 +8,20 @@ const scoreBox = document.querySelector('.score');
 const accuracyBox = document.querySelector('.accuracy');
 const timerBox = document.querySelector('.timer');
 const title = document.querySelector('.title')
-const HTML = document.getElementsByTagName('html')
+
+// ^^^initializing all element references that I will use in order to create the web app
 
 let accuracyValues = [];
 let scoreValue = 0;
 let timeLeft = 30;
 let onClick;
 let intervalFunction;
+
+// ^^^initializing all variables that will change based on user input or a timer
+// accuracyValues will hold an array of the accuracy values of each click
+// scoreValue will keep track of current score of user
+// onClick will store the function that will calculate the accuracy value and score on each click, along with changing the targets location
+// intervalFunction will store essentially the timer, calling a function to decrement the value of timeLeft by one on each interval
 
 const accuracyCalc = (accuracy) => {
     if(timeLeft === 0) return;
@@ -27,6 +34,13 @@ const accuracyCalc = (accuracy) => {
     return accuracyBox.innerText = `Accuracy: ${avgAccuracy}%`;
 }
 
+// ^^^ accuracyCalc will first check to see if there is still time left.
+// if so, accuracyCalc will take the accuracy argument and push it into the accuracyValues array.
+// the average accuracy is calculated by taking the sum of all values within the accuracyValues array and
+// dividing it by its length. avgAccuracy is equal to this calculated average to 1 decimal point.
+// finally the innerText property of the element referenced in accuracyBox is set equal to `Accuracy: (the object literal of avgAccuracy)`
+// Thus constantly updating the displayed accuracy value per click
+
 const scoreCalc = () => {
     if(timeLeft === 0) {
         return;
@@ -35,6 +49,11 @@ const scoreCalc = () => {
     scoreBox.innerText = `Score: ${scoreValue}`;
     }
 }
+
+// ^^^ scoreCalc will first set the timerBox innerText property equal to `Timer: (the object literal of timeLeft)`.
+// Then it will check to see if there is still time left. If so, the value of the variable scoreValue will increment by 1,
+// along with the innerText property of scoreBox set equal to `Score: (the object literal of scoreValue)`
+// Thus constantly updating the displayed score value per click
 
 const minusOneSecond = () => {
     timerBox.innerText = `Timer: ${timeLeft} seconds left`;
