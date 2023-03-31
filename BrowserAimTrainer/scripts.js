697b8b1290c4106a0d56c5ec7efce4d2c7d05007
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-03-30 22:47:01 -0500

    got too carried away making the project, forgot to push changes often, regardless, got 80% of the program to work, a countdown timer from 30 seconds, a score counter that adds 1 point per time clicked on the target, and the target moving to a random position every time it is clicked. Need to fix the accuracy calculation and display it, otherwise it works better than I expected

--- BrowserAimTrainer/scripts.js
+++ BrowserAimTrainer/scripts.js
@@ -1,10 +1,93 @@
-const centerDot = document.getElementsByClassName('.centerDot');
-const middleRing = document.getElementsByClassName('.middleRing');
-const outerRing = document.getElementsByClassName('.outerRing');
-const targetArea = document.getElementsByClassName('.targetArea')''
+const centerDot = document.querySelector('.centerDot');
+const middleRing = document.querySelector('.middleRing');
+const outerRing = document.querySelector('.outerRing');
+const targetContainer = document.querySelector('.targetContainer');
+const targetArea = document.querySelector('.targetArea');
+const scoreboardContainer = document.querySelector('.scoreboardContainer');
+const scoreBox = document.querySelector('.score');
+const accuracyBox = document.querySelector('.accuracy');
+const timerBox = document.querySelector('.timer');
 
-const onClick = (event) => {
+console.log(centerDot);
 
+let accuracyValues = [];
+let scoreValue = 0
+
+const accuracyCalc = (accuracy) => {
+    accuracyValues.push(accuracy);
+    let SumAccuracyValues = accuracyValues.reduce( (accumulator, currentValue) => {
+        accumulator + currentValue;
+    });
+    let avgAccuracy = SumAccuracyValues/accuracyValues.length;
+    return accuracyBox.innerText = `Accuracy: ${avgAccuracy}%`;
 }
 
-document.addEventListener('click', onClick(e));
+const scoreCalc = () => {
+    scoreValue++;
+    scoreBox.innerText = `Score: ${scoreValue}`;
+}
+
+let timeLeft = 30;
+let onClick;
+let intervalFunction;
+
+const onStart = (event) => {
+    if(onClick !== undefined) return;
+    onClick = (arg) => {
+
+        let elementClicked = arg.target;
+        console.log(elementClicked);
+        console.log(centerDot);
+        if(elementClicked === centerDot) {
+            moveTheTarget();
+            accuracyCalc(100);
+            scoreCalc();
+        } else if(elementClicked === middleRing) {
+            moveTheTarget();
+            accuracyCalc(75);
+            scoreCalc();
+        } else if(elementClicked === outerRing) {
+            moveTheTarget();
+            accuracyCalc(50);
+            scoreCalc();
+        } else if(elementClicked === targetArea) {
+            moveTheTarget();
+            accuracyCalc(0);
+        } else {
+            
+            return;
+    
+        };
+    
+    }
+    document.addEventListener('click', (e) => {
+        onClick(e);
+    })
+    intervalFunction = setInterval(minusOneSecond, 1000);
+    
+}
+
+const minusOneSecond = () => {
+    timerBox.innerText = `Timer: ${timeLeft} seconds left`;
+    if(timeLeft === 0) {
+        return clearInterval(intervalFunction);
+    }
+    timeLeft--;
+}
+
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+}
+
+
+const moveTheTarget = () => {
+    targetContainer.style.top = getRandomInt(600) + 'px';
+    targetContainer.style.left = getRandomInt(600) + 'px';
+}
+
+document.addEventListener('click', (e) => {
+    onStart(e);
+});
+
+
+
