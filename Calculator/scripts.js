0c1de988834c13e02784c5c0a81447cd16b9886a
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-04-03 01:47:48 -0500

    changed some styling elements in order to make a more accurate portrayal of the real like calculator model TOSHIBA BC-8111

--- Calculator/scripts.js
+++ Calculator/scripts.js
@@ -10,26 +10,46 @@ const blueLabels = [5, 9, 10, 14, 15, 19, 20, 23, 24];
 
 const buttonContructor = (object) => {
     for (let i = 0; i < arrayOfButtons.length; i++) {
-        let buttonBackground = document.createElement('div');
+
+            let buttonBackground = document.createElement('div');
+
+        if(i === 0) {
+            buttonBackground.setAttribute('class', 'onAndOffBackground');
+        } else {
             buttonBackground.setAttribute('class', 'buttonBackground');
-        let buttonLabel = document.createElement('button');
-            buttonLabel.setAttribute('class', 'buttonLabel')
-            if(i === 0) {
-                buttonLabel.classList.add('onAndOffButton')
-            } else if(redLabels.includes(i)){
-                buttonLabel.classList.add('red')
-            } else if(blueLabels.includes(i)) {
-                buttonLabel.classList.add('blue')
-            } else {
-                buttonLabel.classList.add('black')
-            }
-        let textLabel = document.createElement('h4');
-        textLabel.setAttribute('class', 'textLabel');
-        textLabel.innerText = object[i].buttonLabel;
-        buttonLabel.appendChild(textLabel);
-        buttonBackground.appendChild(buttonLabel);
-        buttonContainer.appendChild(buttonBackground);
-    }
+        };
+
+            let buttonLabel = document.createElement('button');
+
+        if(i === 0) {
+            buttonLabel.setAttribute('class', 'onAndOffButtonLabel')
+        } else {
+            buttonLabel.setAttribute('class', 'buttonLabel');
+        };
+
+        buttonLabel.classList.add(`${object.buttonLabel}`);
+
+        if(redLabels.includes(i)){
+            buttonLabel.classList.add('red')
+        } else if(blueLabels.includes(i)) {
+            buttonLabel.classList.add('blue')
+        } else {
+            buttonLabel.classList.add('black')
+        };
+
+            let buttonText = document.createElement('h4');
+            buttonText.setAttribute('class', 'textLabel');
+            buttonText.innerText = object[i].buttonLabel;
+            buttonLabel.appendChild(buttonText);
+            buttonBackground.appendChild(buttonLabel);
+            buttonContainer.appendChild(buttonBackground);
+
+    };
+
 }
 
 buttonContructor(arrayOfButtons);
+
+document.addEventListener('mousedown', (e) => {
+
+})
