2f7455481453558d8cab9af68edd758dce35e1b1
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-04-04 04:05:05 -0500

    DOM manipulated to display calc buttons, building logic script for calculator, added an on/off conditional so that the calculator will only work if it is turned on, also gave calculator logic script a function wrapper so the logic wont run until async data is fetched first

--- Calculator/scripts.js
+++ Calculator/scripts.js
@@ -13,7 +13,7 @@ async function fetchButtonFile() {
     redButtonIndex = data.redButtonIndex;
     blueButtonIndex = data.blueButtonIndex;
 
-}
+};
 
 const buttonMaker = (array) => {
     
@@ -43,15 +43,110 @@ const buttonMaker = (array) => {
         buttonBackground.appendChild(button);
         buttonContainer.appendChild(buttonBackground);
 
-    }
+    };
 
-}
+};
 
 fetchButtonFile().then(() => {
     buttonMaker(buttons);
+    calculator();
 });
 
+const calculator = () => {
 
-document.addEventListener('mousedown', (e) => {
+const bottomStructureBlock = document.querySelector('.bottomStructureBlock');
+const onOrOffButton = document.querySelector('.onAndOffButtonLabel')
+const output = document.querySelector('.output');
 
-})
+let onOrOffValue = 0;
+let outputValue;
+
+const calculatorFunctions = {
+    "CM": cmFunction,
+    "RM": rmFunction,
+    "M-": mMinusFunction,
+    "M+": mPlusFunction,
+    "√": sqrtFunction,
+    "7": sevenFunction,
+    "8": eightFunction,
+    "9": nineFunction,
+    "-": minusFunction,
+    "%": percentFunction,
+    "4": fourFunction,
+    "5": fiveFunction,
+    "6": sixFunction,
+    "÷": divideFunction,
+    "π": piFunction,
+    "1": oneFunction,
+    "2": twoFunction,
+    "3": threeFunction,
+    "x": multiplyFunction,
+    "C": clearFunction,
+    "0": zeroFunction,
+    ".": decimalFunction,
+    "=": equalFunction,
+    "+": addFunction
+}
+
+const calculatorOnOrOff = (event) => {
+    
+    if(onOrOffValue === 0) {
+        return output.innerText = '';
+    } else {
+        calculatorLogic(event);
+    };
+
+};
+
+
+const calculatorLogic = (event) => {
+
+}
+
+const add = () => {
+
+}
+
+const subtract = () => {
+
+}
+
+const multiply = () => {
+
+}
+
+const divide = () => {
+
+}
+
+const equals = () => {
+
+}
+
+const sqrt = () => {
+
+}
+
+const clear = () => {
+
+}
+
+const percent = () => {
+
+}
+
+onOrOffButton.addEventListener('click', () => {
+    if(onOrOffValue === 0) {
+        onOrOffValue += 1;
+        console.log(onOrOffValue)
+    } else {
+        onOrOffValue -=1;
+        console.log(onOrOffValue)
+    };
+});
+
+bottomStructureBlock.addEventListener('click', (e) => {
+    calculatorOnOrOff(e);
+});
+
+}
