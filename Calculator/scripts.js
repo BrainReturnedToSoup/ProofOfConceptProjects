f4191d37a203a94a9aa3ccde5d4b0e61d9ff2fb8
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-04-05 00:35:13 -0500

    removed text label element completely, experimenting with various structures for the calculation logic

--- Calculator/scripts.js
+++ Calculator/scripts.js
@@ -4,6 +4,12 @@ let blueButtonIndex = [];
 
 const buttonContainer = document.querySelector('.buttonContainer');
 
+let onAndOffFunction;
+let specialClicked;
+let numberBuilderClicked;
+let operatorClicked;
+let calculationConstructor;
+
 async function fetchButtonFile() {
 
     const response = await fetch('./buttons.json');
@@ -28,18 +34,31 @@ const buttonMaker = (array) => {
         if(i === 0) {
             buttonBackground.classList.add('onAndOffBackground');
             button.classList.add('onAndOffButtonLabel');
+            button.addEventListener('click', onAndOffFunction);
         } else {
             buttonBackground.classList.add('buttonBackground');
             button.classList.add('buttonLabel');
         }
 
-        redButtonIndex.includes(i) ? button.classList.add('red') : blueButtonIndex.includes(i) ? button.classList.add('blue') : button.classList.add('black');
+        if(redButtonIndex.includes(i)) {
+            button.classList.add('red');
+            button.addEventListener('click', (e) => {
+                specialClicked(e);
+            });
+        } else if(blueButtonIndex.includes(i)) {
+            button.classList.add('blue');
+            button.addEventListener('click', (e) => {
+                operatorClicked(e);
+            });
+        } else {
+            button.classList.add('black');
+            button.addEventListener('click', (e) => {
+                numberBuilderClicked(e);
+            });
+        }
+ 
+        button.innerText = array[i];
 
-        textOnButton.classList.add('textLabel');
-        textOnButton.innerText = array[i];
-
-
-        button.appendChild(textOnButton);
         buttonBackground.appendChild(button);
         buttonContainer.appendChild(buttonBackground);
 
@@ -58,95 +77,97 @@ const bottomStructureBlock = document.querySelector('.bottomStructureBlock');
 const onOrOffButton = document.querySelector('.onAndOffButtonLabel')
 const output = document.querySelector('.output');
 
-let onOrOffValue = 0;
-let outputValue;
+let onOrOff = 0;
+let outputPercent = 0;
 
-const calculatorFunctions = {
-    "CM": cmFunction,
-    "RM": rmFunction,
-    "M-": mMinusFunction,
-    "M+": mPlusFunction,
-    "√": sqrtFunction,
-    "7": sevenFunction,
-    "8": eightFunction,
-    "9": nineFunction,
-    "-": minusFunction,
-    "%": percentFunction,
-    "4": fourFunction,
-    "5": fiveFunction,
-    "6": sixFunction,
-    "÷": divideFunction,
-    "π": piFunction,
-    "1": oneFunction,
-    "2": twoFunction,
-    "3": threeFunction,
-    "x": multiplyFunction,
-    "C": clearFunction,
-    "0": zeroFunction,
-    ".": decimalFunction,
-    "=": equalFunction,
-    "+": addFunction
-}
-
-const calculatorOnOrOff = (event) => {
-    
-    if(onOrOffValue === 0) {
-        return output.innerText = '';
+onAndOffFunction = () => {
+    if(onOrOff === 0) {
+        onOrOff++;
+        console.log('on');
     } else {
-        calculatorLogic(event);
-    };
-
+        onOrOff--;
+        console.log('off');
+    }
 };
 
+let operationArray = [];
 
-const calculatorLogic = (event) => {
-
+const calculationContructor = (array) => {
+    console.log(array)
 }
 
-const add = () => {
+specialClicked = (event) => {
+    
+    if(onOrOff === 0) return;
 
+    let idOfElement = event.target.id
+        if(idOfElement === 'CM') {
+
+        } else if(idOfElement === 'RM') {
+
+        } else if(idOfElement === 'M-') {
+
+        } else if(idOfElement === 'M+') {
+
+        };
 }
 
-const subtract = () => {
+operatorClicked = (event) => {
+        
+    if(onOrOff === 0) return;
 
+    let idOfElement = event.target.id;
+        if(idOfElement === '√') {
+            operationArray.push('Math.sqrt');
+        } else if(idOfElement === '-') {
+            operationArray.push('-');
+        } else if(idOfElement === '%') {
+            if(outputPercent === 0) {
+                outputPercent++;
+            } else {
+                outputPercent--;
+            }
+        } else if(idOfElement === '÷') {
+            operationArray.push('-');
+        } else if(idOfElement === 'π') {
+            operationArray.push('Math.PI');
+        } else if(idOfElement === 'x') {
+            operationArray.push('*');
+        } else if(idOfElement === 'C') {
+            operationArray = [];
+        } else if(idOfElement === '=') {
+            calculationConstructor(operationArray);
+        } else if( idOfElement === '+') {
+            operationArray.push('+');
+        }
 }
 
-const multiply = () => {
+numberBuilderClicked = (event) => {
+        
+    if(onOrOff === 0) return;
 
-}
-
-const divide = () => {
-
-}
-
-const equals = () => {
-
-}
-
-const sqrt = () => {
-
-}
-
-const clear = () => {
-
-}
-
-const percent = () => {
-
-}
-
-onOrOffButton.addEventListener('click', () => {
-    if(onOrOffValue === 0) {
-        onOrOffValue += 1;
-        console.log(onOrOffValue)
-    } else {
-        onOrOffValue -=1;
-        console.log(onOrOffValue)
+    let idOfElement = event.target.id;
+        if(idOfElement === '1') {
+            operationArray.push('1');
+        } else if(idOfElement === '2') {
+            operationArray.push('2');
+        } else if(idOfElement === '3') {
+            operationArray.push('3');
+        } else if(idOfElement === '4') {
+            operationArray.push('4');
+        } else if(idOfElement === '5') {
+            operationArray.push('5');
+        } else if(idOfElement === '6') {
+            operationArray.push('6');
+        } else if(idOfElement === '7') {
+            operationArray.push('7');
+        } else if(idOfElement === '8') {
+            operationArray.push('8');
+        } else if(idOfElement === '9') {
+            operationArray.push('9');
+        } else if(idOfElement === '.') {
+            operationArray.push('.');
+        }
     };
-});
-
-bottomStructureBlock.addEventListener('click', (e) => {
-    calculatorOnOrOff(e);
-});
 
 }
