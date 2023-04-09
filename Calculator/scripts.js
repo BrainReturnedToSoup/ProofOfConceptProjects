92cca6401a2a75ca6ae1bec8a5352b04a95f6c70
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-04-09 02:13:04 -0500

    saving before I leave, added the function that will handle the event and determine which button was clicked

--- Calculator/scripts.js
+++ Calculator/scripts.js
@@ -1,5 +1,56 @@
 const elementsObj = {};
+
 document.querySelectorAll('button').forEach( element => {
     elementsObj[`${element.getAttribute('data-id')}`] = element;
 })
-console.log(elementsObj);
+
+document.querySelector('.buttonContainer').addEventListener('click', whichButton(e))
+
+function whichButton(event) {
+    const dataId = event.target.getAttribute('data-id');
+    if(dataId != undefined) {
+        switch(dataId) {
+            case 'percent':
+
+            case 'sqrt':
+
+            case 'clear':
+
+            case 'backspace':
+
+            case 'add':
+
+            case 'subtract':
+
+            case 'multiply':
+
+            case 'divide':
+
+            case 'seven':
+
+            case 'eigth':
+
+            case 'nine':
+
+            case 'decimal':
+
+            case 'four':
+
+            case 'five':
+
+            case 'six':
+
+            case 'zero':
+
+            case 'one':
+
+            case 'two':
+
+            case 'three':
+
+            case 'equal':
+        }
+    } else {
+        return;
+    }
+}
