eddfd12091012a6185b3348ba42f2d4c8a7e1da8
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-05-03 16:49:13 -0500

    creating various script files in order to modulate various functionalit

---
+++ Library/scripts/nav-bar-button-functionality.js
@@ -0,0 +1,17 @@
+const navBarButtons = {
+    removeBookButton: document.querySelector('.Remove-Book'),
+    addBookButton:  document.querySelector('.Add-Book'),
+    newLibraryButton:   document.querySelector('.New-Library')
+}
+
+function removeBookHandler () {
+
+}
+
+function addBookHandler () {
+
+}
+
+function newLibraryHander() {
+    
+}
