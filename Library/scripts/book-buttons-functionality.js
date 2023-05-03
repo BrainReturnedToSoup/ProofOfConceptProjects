49b60b26428386e2896250c87755f8155e0dedd0
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-05-03 16:58:38 -0500

    rounding out the plan for how I am going to structure the scripts, adding empty handler functions, along with referencing important buttons etc

---
+++ Library/scripts/book-buttons-functionality.js
@@ -0,0 +1,17 @@
+const bookCardButtons = {
+    deleteCardButton: document.querySelector('.X-Button'),
+    addPageReadButton: document.querySelector('.Increment-Page-Value'),
+    subPageReadButton: document.querySelector('.Decrement-Page-Value')
+}
+
+function deleteCardHander () {
+
+}
+
+function addPageHander() {
+
+}
+
+function subPageHandler() {
+    
+}
