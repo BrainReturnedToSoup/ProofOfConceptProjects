ec6e7a1b4bcbd716ebdc33d81a2172cefd30e5cc
NikkolasMinton <minton.nikkolas@gmail.com>
2023-05-14 20:15:30 -0500

    made the DOM refs more concise and clear about their purpose and structure

--- TicTacToe/scripts/RefsDOM.js
+++ TicTacToe/scripts/RefsDOM.js
@@ -1,8 +1,14 @@
 const bodyElement = document.querySelector("body");
 
 const ticTacToeContainer = bodyElement.children[0],
+  //TicTacToe-Container
+
   ticTacToeCells = ticTacToeContainer.children,
+  //TicTacToe-Cell list
+
   individualCellList = {};
+
+  //Cell-0: refToCellWithClassEqualToKey
 
 for (let i = 0; i < ticTacToeCells.length; i++) {
   individualCellList[`Cell-${i}`] = ticTacToeCells[i];
