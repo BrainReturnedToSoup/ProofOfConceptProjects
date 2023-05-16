aea981dce432f545d4db87b4ebcb1675b093ccf6
NikkolasMinton <minton.nikkolas@gmail.com>
2023-05-15 20:53:52 -0500

    filling in the code blocks of the easiest methods for the game, then working on the logic for the more complex methods to control the game

--- TicTacToe/scripts/GameStateManager.js
+++ TicTacToe/scripts/GameStateManager.js
@@ -1,18 +1,29 @@
 export class TicTacToeState {
   constructor() {
+    this.gameExecuting = false;
+    this.playerSymbol = "X";
+    this.computerSymbol = "O";
     this.currentState = [
       ["-", "-", "-"],
       ["-", "-", "-"],
       ["-", "-", "-"],
     ];
     this.availableCells = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    this.gameExecuting = false;
-    this.playerSymbol = "X";
   }
 
   #checkState() {}
 
-  #resetState() {}
+  #resetState() {
+    this.gameExecuting = false;
+    this.playerSymbol = "X";
+    this.computerSymbol = "O";
+    this.currentState = [
+      ["-", "-", "-"],
+      ["-", "-", "-"],
+      ["-", "-", "-"],
+    ];
+    this.availableCells = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  }
 
   #computerPickCell() {
     //use minimax algo to make unbeatable bot
@@ -20,15 +31,50 @@ export class TicTacToeState {
 
   #determineWinner() {}
 
-  #storeOnLocal() {}
+  #storeOnLocal(winner) {
+    switch (winner) {
+      case "Player":
+        break;
+      case "Computer":
+        break;
+      default:
+    }
+  }
 
-  togglePlayerSymbol() {}
+  togglePlayerSymbol() {
+    if (this.gameExecuting === false) {
+      switch (this.playerSymbol) {
+        case "X":
+          this.playerSymbol = "O";
+          this.computerSymbol = "X";
+          break;
+        case "O":
+          this.playerSymbol = "X";
+          this.computerSymbol = "O";
+          break;
+        default:
+          this.playerSymbol = "X";
+          this.computerSymbol = "O";
+      }
+    }
+  }
 
-  endGame() {}
+  endGame() {
+    if (this.gameExecuting === true) {
+    }
+  }
 
-  startGame() {}
+  startGame() {
+    if (this.gameExecuting === false) {
+    }
+  }
 
-  cellPicked() {}
+  cellPicked() {
+    if (this.gameExecuting === true) {
+    }
+  }
 }
+
+localStorage.setItem("Scores", JSON.stringify([0, 0]));
 
 export const ticTacToeGameState = new TicTacToeState();
