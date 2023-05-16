20651d847f42aab71c9f408b00e93d3724f3ecaa
NikkolasMinton <minton.nikkolas@gmail.com>
2023-05-15 21:57:52 -0500

    creating the checkState class method to accept and array containing various instructions for what state to check, as well as a private object in order to predefine the instructions in a straightforward, easy to use manner for other higher level parts of the program

--- TicTacToe/scripts/GameStateManager.js
+++ TicTacToe/scripts/GameStateManager.js
@@ -3,6 +3,7 @@ export class TicTacToeState {
     this.gameExecuting = false;
     this.playerSymbol = "X";
     this.computerSymbol = "O";
+    this.computersTurn = false;
     this.currentState = [
       ["-", "-", "-"],
       ["-", "-", "-"],
@@ -11,12 +12,45 @@ export class TicTacToeState {
     this.availableCells = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   }
 
-  #checkState() {}
+  #stateMethods = {
+    Check_Execution: function () {},
+    Check_PlayerSymbol: function () {},
+    Check_ComputerSymbol: function () {},
+    Check_Turn: function () {},
+    Check_CurrentState: function () {},
+    Check_AvailableCells: function () {},
+  };
+
+  #checkState(instructions) {
+    //instructions should be an array containing atleast one of these types of strings
+    if (Array.isArray(instructions)) {
+      instructions.forEach((stateMethodKey) =>
+        this.#stateMethods[stateMethodKey]()
+      );
+    }
+  }
+
+  #checkStateInstructions = {
+    checkAllStates: [
+      "Check_Execution",
+      "Check_PlayerSymbol",
+      "Check_ComputerSymbol",
+      "Check_Turn",
+      "Check_CurrentState",
+      "Check_AvailableCells",
+    ],
+    checkTurn: [
+      "Check_Execution",
+      "Check_ComputersTurn",
+      "Check_AvailableCells",
+    ],
+  };
 
   #resetState() {
     this.gameExecuting = false;
     this.playerSymbol = "X";
     this.computerSymbol = "O";
+    this.computersTurn = false;
     this.currentState = [
       ["-", "-", "-"],
       ["-", "-", "-"],
