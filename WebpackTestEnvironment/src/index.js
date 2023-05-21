bb49c0abe6b830785baede539cf36825ba0d2682
NikkolasMinton <minton.nikkolas@gmail.com>
2023-05-21 01:49:14 -0500

    reverting state back to basic webpack template for another tutorial

--- WebpackTestEnvironment/src/index.js
+++ WebpackTestEnvironment/src/index.js
@@ -1,23 +1,10 @@
 import _ from 'lodash';
-import './style.css'
-import Icon from './icon.png'
-import dataXML from './data.xml'
-import dataCSV from './data.csv'
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.log(dataXML);
-    console.log(dataCSV);
-    
+   
     return element;
 }
 
--- WebpackTestEnvironment/webpack.config.js
+++ WebpackTestEnvironment/webpack.config.js
@@ -6,28 +6,4 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|jpg|svg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(CSV|TSV)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-    ],
-  },
 };
