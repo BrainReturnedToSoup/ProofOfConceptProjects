d9b506c07eb880b78592e6ad28e8762b8c270e09
NikkolasMinton <minton.nikkolas@gmail.com>
2023-06-05 19:19:14 -0500

    made the main filtering method, which it will behave based on specific conditions its given to filter out the todo cards accordingly and return an array of the filtered out todo cards

--- TodoList/src/modules/Tasks.js
+++ TodoList/src/modules/Tasks.js
@@ -104,11 +104,8 @@ export class Tasks {
       regularTodoCards = this.#currentAppState.todoInfo.regular;
 
     if (Object.keys(this.#todoCardFilterMethods).includes(selectedOption)) {
-      const selectedTodoCards = this.#todoCardFilterMethods[selectedOption]([
-        regularTodoCards,
-        allProjects,
-      ]);
-      this.#todoCardBuilder(selectedTodoCards);
+      const selectedTodoCards = this.#todoCardFilterMethods[selectedOption]();
+      this.#todoCardFactory.builder(selectedTodoCards);
     } else {
       throw new Error(
         `ERROR: Invalid selected option, received ${selectedOption}`
@@ -120,43 +117,144 @@ export class Tasks {
     const specificProject = this.#currentAppState.selectedOption["project"],
       selectedTodoCards =
         this.#todoCardFilterMethods["Project"](specificProject);
-    this.#todoCardBuilder(selectedTodoCards);
+    this.#todoCardFactory.builder(selectedTodoCards);
   }
 
   #todoCardFilterMethods = {
-    Inbox: (todoCardsRefArr) => {},
+    Inbox: function () {
+      const allCards = this.mainFilter("All");
+    },
     //filtering for all existing cards
-    Today: (todoCardsRefArr) => {},
-    //filtering for cards that fall within the created today category
-    "This Week": (todoCardsRefArr) => {},
-    //filtering for cards that fall within the created today category
-    Project: (specificProject) => {},
-    //filtering for cards that fall within the created today category
-    mainFilter: (condition) => {},
+    Today: function () {
+      const cardsWithinToday = this.mainFilter("All", "Today");
+    },
+    //filtering for cards within the app state that fall within the created today category
+    "This Week": function () {
+      const cardsWithinThisWeek = this.mainFilter("All", "This Week");
+    },
+    //filtering for cards within the app state that fall within the created this week category
+    Project: function (specificProject) {
+      const selectedOptionTarget = this.grabCurrentSelectedOptionTarget(),
+        cardsWithinToday = this.mainFilter(
+          "All",
+          "Project",
+          selectedOptionTarget
+        );
+    },
+    //filtering for cards within the app state that fall within a specific selected project category
+    mainFilter: (command, inputCondition, targetProjArg) => {
+      const { projects, regular } = this.#currentAppState.todoInfo,
+        currentDate = new Date();
+      let foundCards = [],
+        comparedDayValue = null,
+        targetProject = null;
+
+      switch (inputCondition) {
+        case "Today":
+          comparedDayValue = 1;
+          break;
+        case "This Week":
+          comparedDayValue = 7;
+          break;
+        case "Project":
+          if (targetProjArg !== undefined) {
+            targetProject = targetProjArg;
+          } else {
+            throw new Error("ERROR: Invalid target project argument");
+          }
+          break;
+        default:
+          throw new Error("ERROR: Invalid input condition");
+      }
+
+      if (command === "All") {
+        for (let todoCard in regular) {
+          if (comparedDayValue === null && targetProject === null) {
+            foundCards.push(regular[todoCard]);
+            //pushes all non project cards into the foundCards array if the inbox selection is selected
+          } else if (
+            typeof comparedDayValue === "number" &&
+            targetProject === null
+          ) {
+            const fallsWithinTimeRange =
+              comparedDayValue >
+              this.#dateDifferenceInDays(currentDate, regular[todoCard].date);
+            if (fallsWithinTimeRange) {
+              foundCards.push(regular[todoCard]);
+              //pushes the todo cards that fall within the defined timeframe into the foundCards array
+            }
+          }
+        }
+      }
+
+      if (command === "All" || command === "ProjectsOnly") {
+        for (let project in projects) {
+          for (let todoCard in projects[project]) {
+            if (comparedDayValue === null && targetProject === null) {
+              foundCards.push(projects[project][todoCard]);
+              //pushes all project todo cards that exist within all projects into the foundCards array
+            } else if (
+              typeof targetProject === "string" &&
+              projects[project] === projects[targetProject] &&
+              comparedDayValue === null
+            ) {
+              //checking to see if a target project has been defined, and if the current project being iterated through is the
+              //corresponding project, and if there isn't a compared day timeframe value, thus pushing all todo cards within this
+              //specific project into the array
+              foundCards.push(projects[project][todoCard]);
+            } else if (
+              typeof comparedDayValue === "number" &&
+              targetProject === null
+            ) {
+              const fallsWithinTimeRange =
+                comparedDayValue >
+                this.#dateDifferenceInDays(currentDate, regular[todoCard].date);
+              if (fallsWithinTimeRange) {
+                foundCards.push(regular[todoCard]);
+                //pushes the todo cards that fall within the defined timeframe into the foundCards array
+              }
+            }
+          }
+        }
+      }
+      return foundCards;
+    },
+    //general iterator and filter method that the others will use, behavior defined by its args supplied
+    grabCurrentSelectedOptionTarget: () => {
+      if (this.#currentAppState.selectedOption?.["project"]) {
+        return this.#currentAppState.selectedOption["project"];
+      } else {
+        return this.#currentAppState.selectedOption;
+      }
+    },
   };
 
-  #dateDifferenceInDays(referenceDate, todoCardDate) {
+  #dateDifferenceInDays = (referenceDate, todoCardDate) => {
     return (
       (referenceDate.getTime() - todoCardDate.getTime()) / (24 * 60 * 60 * 1000)
     );
-  }
+  };
+  #todoCardFactory = {
+    builder: function (selectedTodoCardsArr) {
+      for (let todoCard of selectedTodoCardsArr) {
+        const range = document.createRange(),
+          todoCardFrag = range.createContextualFragment(
+            this.#DOMtemplates.todoCard
+          ),
+          todoCardText = Object.keys(todoCard)[0],
+          todoCardDate = todoCard[todoCardText].date,
+          todoCardDone = todoCard[todoCardText].done,
+          textContainer = todoCardFrag.querySelector(
+            ".Todo-Card-Left-Container"
+          );
 
-  #todoCardBuilder(selectedTodoCardsArr) {
-    for (let todoCard of selectedTodoCardsArr) {
-      const range = document.createRange(),
-        todoCardFrag = range.createContextualFragment(
-          this.#DOMtemplates.todoCard
-        ),
-        todoCardText = Object.keys(todoCard)[0],
-        todoCardDate = todoCard[todoCardText].date,
-        todoCardDone = todoCard[todoCardText].done,
-        textContainer = todoCardFrag.querySelector(".Todo-Card-Left-Container");
+        textContainer.textContent = todoCardText;
 
-      textContainer.textContent = todoCardText;
-
-      this.#DOMcache.todoCardListElement.append(todoCardFrag);
-    }
-  }
+        this.#DOMcache.todoCardListElement.append(todoCardFrag);
+      }
+    },
+    appendToDOM: () => {},
+  };
 
   #eventListenerLogic(event) {
     const targetClassList = Array.from(event.target.classList);
