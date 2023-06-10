5f5a863d3c65acbc4577fb20923d49a8544153ae
NikkolasMinton <minton.nikkolas@gmail.com>
2023-06-10 17:50:59 -0500

    reseting the sandbox so I can practice sliders

--- DynamicAnimationSandbox/src/index.js
+++ DynamicAnimationSandbox/src/index.js
@@ -1,11 +1,8 @@
 import './stylesheets/css-reset.css'
-import { Navbar } from "./modules/navbar.js";
 
 const mainPageModules = {
-    navbar: new Navbar(),
   },
   startApp = () => {
-    mainPageModules.navbar.init();
   };
 
 startApp();
