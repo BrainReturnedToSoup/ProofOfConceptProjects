d11cbea733e7b4837447e12a855da0f49ff26e31
NikkolasMinton <minton.nikkolas@gmail.com>
2023-06-10 23:57:22 -0500

    made it so that the slider can be initialized with a predefined number of images within the slider each with their own unique identifier within the sequence as well as the entire module produced requires a unique identifieritself

--- DynamicAnimationSandbox/src/modules/ImageSlider.js
+++ DynamicAnimationSandbox/src/modules/ImageSlider.js
@@ -4,29 +4,37 @@ class ImageSlider {
   };
   #DOMtemplates = {
     sliderContainer: `
-      <div class="Image-Slider-Container"></div>
+      <div class="Image-Slider-Container ${this.#uniqueIdentifier}"></div>
     `,
     sliderDisplay: `
-      <div class="Image-Slider-Display">
-        <div class="Image-Slider-Image Image-1"></div>
-        <div class="Image-Slider-Image Image-2"></div>
-        <div class="Image-Slider-Image Image-3"></div>
-      </div>
+      <div class="Image-Slider-Display ${this.#uniqueIdentifier}"></div>
+    `,
+    sliderImage: `
+    <div class="Image-Slider-Image ${this.#uniqueIdentifier}"></div>
     `,
     sliderButtons: `
-      <div class="Image-Slider-Buttons">
-        <button class="Image-Slider-Button Left"></button>
-        <button class="Image-Slider-Button Reset"></button>
-        <button class="Image-Slider-Button Right"></button>
+      <div class="Image-Slider-Buttons ${this.#uniqueIdentifier}">
+        <button class="Image-Slider-Button ${
+          this.#uniqueIdentifier
+        } Left"></button>
+        <button class="Image-Slider-Button ${
+          this.#uniqueIdentifier
+        } Reset"></button>
+        <button class="Image-Slider-Button ${
+          this.#uniqueIdentifier
+        } Right"></button>
       </div>
     `,
   };
 
   #DOMSliderImageClasses = ["Image-1", "Image-2", "Image-3"];
-
   #arrCurrentKeySIC = 0;
+
   #userClickedSliderButton = false;
   #autoTransitionInterval = null;
+
+  #uniqueIdentifier = "";
+  #uniqueIdentifierCache = [];
 
   #initSliderAutoTransition() {
     if (!this.#autoTransitionInterval) {
@@ -36,34 +44,121 @@ class ImageSlider {
     }
   }
 
-  #moveSliderRight() {
-
-  }
-  #moveSliderLeft() {
-
-  }
-  #resetSliderPosition() {
-
-  }
+  #moveSliderRight() {}
+  #moveSliderLeft() {}
+  #resetSliderPosition() {}
 
   #sliderButtonLogic(event) {}
 
-  #removeAllEventListeners() {}
+  #removeAllEventListeners(assembledSlider) {}
 
-  #addAllEventListeners() {}
+  #addAllEventListeners(assembledSlider) {}
 
-  initializeEventListeners(sliderFrag) {}
+  initializeEventListeners(assembledSlider) {
+    this.#removeAllEventListeners(assembledSlider);
+    this.#addAllEventListeners(assembledSlider);
+  }
 
-  #buildSlider() {}
+  #buildCompleteSlider() {
+    const sliderContainer = this.#buildContainer(),
+      sliderDisplay = this.#buildDisplay(),
+      sliderButtons = this.#buildButtons();
 
-  #appendSlider(parentElement) {}
+    let assembledSlider = sliderContainer;
+    assembledSlider.appendChild(sliderDisplay);
+    assembledSlider.appendChild(sliderButtons);
 
-  init(parentElement) {
-    if (parentElement) {
-      const sliderFrag = this.#buildSlider();
+    return assembledSlider;
+  }
 
-      this.initializeEventListeners(sliderFrag);
-      parentElement.append(sliderFrag);
+  #buildContainer() {
+    const SCrange = document.createRange(),
+      sliderContainerFrag = SCrange.createContextualFragment(
+        this.#DOMtemplates.sliderContainer
+      );
+    this.#DOMcache.sliderContainerElement = sliderContainerFrag.querySelector(
+      ".Image-Slider-Container"
+    );
+    return sliderContainerFrag;
+  }
+  #buildDisplay() {
+    const SDrange = document.createRange(),
+      sliderDisplayFrag = SDrange.createContextualFragment(
+        this.#DOMtemplates.sliderDisplay
+      );
+
+    this.#DOMcache.sliderDisplayContainerElement =
+      sliderDisplayFrag.querySelector(".Image-Slider-Display");
+
+    return sliderDisplayFrag;
+  }
+
+  #buildButtons() {
+    const SBrange = document.createRange(),
+      sliderButtonsFrag = SBrange.createContextualFragment(
+        this.#DOMtemplates.sliderButtons
+      );
+    this.#DOMcache.sliderButtonsContainerElement =
+      sliderButtonsFrag.querySelector(".Image-Slider-Buttons");
+    return sliderButtonsFrag;
+  }
+
+  #buildImage() {
+    const SIrange = document.createRange(),
+      sliderImage = SIrange.createContextualFragment(
+        this.#DOMtemplates.sliderImage
+      );
+    return sliderImage;
+  }
+
+  #appendSlider(parentElement, assembledSlider) {
+    parentElement.appendChild(assembledSlider);
+  }
+
+  #mapImageIdentifiers(imageSet) {
+
+    imageSet.forEach((imageElement, index = 0) => {
+      const imageIdentifier = `Image-${index + 1}`;
+      imageElement.classList.add(imageIdentifier);
+      this.#DOMcache[imageIdentifier] = imageElement;
+      this.#DOMSliderImageClasses.push(imageIdentifier);
+    });
+    
+  }
+
+  #addImageElements(numOfImages) {
+    const createdImageElements = [];
+
+    if (numOfImages > 1) {
+      const { sliderDisplayContainerElement } = this.#DOMcache;
+      sliderDisplayContainerElement.innerHTML = "";
+
+      for (let i = 0; i < numOfImages; i++) {
+        const imageElement = this.#buildImage();
+        createdImageElements.push(imageElement);
+        sliderDisplayContainerElement.appendChild(imageElement);
+      }
+    }
+
+    if (createdImageElements.length > 0) {
+      this.#mapImageIdentifiers(createdImageElements);
+    }
+  }
+
+  init(parentElement, uniqueIdentifier, numOfImages = 2) {
+    if (
+      parentElement &&
+      uniqueIdentifier !== null &&
+      !this.#uniqueIdentifierCache.includes(uniqueIdentifier)
+    ) {
+      this.#uniqueIdentifier = uniqueIdentifier;
+
+      const assembledSlider = this.#buildCompleteSlider();
+      this.initializeEventListeners(assembledSlider);
+
+      this.#addImageElements(numOfImages);
+
+      this.#appendSlider(parentElement, assembledSlider);
     }
   }
 }
