8528e022c9bb1921e0a419f6e70c81803dd2697e
NikkolasMinton <minton.nikkolas@gmail.com>
2023-06-12 04:10:12 -0500

    added comments in order to explain the use behind every component within the module

--- DynamicAnimationSandbox/src/modules/ImageSlider.js
+++ DynamicAnimationSandbox/src/modules/ImageSlider.js
@@ -7,14 +7,16 @@ export class ImageSlider {
     intervalTime = 1000,
     resetIntervalTime = 150
   ) {
-    if (numOfImages >= 2 && intervalTime >= 1000) {
+    if (numOfImages >= 2 && intervalTime >= 1000 && resetIntervalTime >= 25) {
       this.#stateData.uniqueIdentifier = uniqueIdentifier;
       this.#stateData.numOfImages = numOfImages;
       this.#stateData.intervalTime = intervalTime;
       this.#stateData.resetIntervalTime = resetIntervalTime;
+      //attributes that will dictate certain behaviors of this image slider instance
     }
   }
   #DOMcache = {};
+  //for saving all created elements in the DOM cache when it's convenient to minimize long run element querying
   #DOMtemplates = {
     sliderContainer: `
       <div class="Image-Slider-Container"></div>
@@ -33,6 +35,7 @@ export class ImageSlider {
       </div>
     `,
   };
+  //various string templates used to create the foundation of the image slider
 
   #stateData = {
     DOMSliderImageClasses: [],
@@ -48,6 +51,10 @@ export class ImageSlider {
     resetIntervalRef: null,
   };
 
+  //used to manage important data in terms of defining the behavior of the image slider as a whole
+  //as well as used as a data reference in order to facilitate specific logic in various methods within
+  //this class
+
   #initSliderAutoTransition() {
     if (!this.#stateData.autoTransitionInterval) {
       this.#stateData.autoTransitionInterval = setInterval(() => {
@@ -55,6 +62,8 @@ export class ImageSlider {
       }, this.#stateData.intervalTime);
     }
   }
+  //method for initializing the automatic image sliding animation, which will always slide right on the set interval
+  //unless interupted by event logic
 
   #moveSliderRight() {
     const currentSelectedImage =
@@ -107,6 +116,12 @@ export class ImageSlider {
     this.#DOMcache[currentSelectedImage].classList.add("Unselected-Right");
     this.#DOMcache[nextSelectedImage].classList.add("Selected-Right");
   }
+  //used to facilitate the concept of moving the current viewed image to the next image
+  //Which the way to create this animation using this logic is to utilize the various selected/unselected
+  //classes, which all this method does is label the next image with the corresponding selected class in order
+  //to animate it on screen, and label the image that was replaced with the corresponding unselected class
+  //in order to animate it off screen. This method will always assign these classes in a fixed order.
+
   #moveSliderLeft() {
     const currentSelectedImage =
       this.#stateData.DOMSliderImageClasses[this.#stateData.arrCurrentKeySIC];
@@ -153,6 +168,9 @@ export class ImageSlider {
     this.#DOMcache[currentSelectedImage].classList.add("Unselected-Left");
     this.#DOMcache[nextSelectedImage].classList.add("Selected-Left");
   }
+
+  //works the same way that the moveSliderRight does, but instead of using the necessary classes to facilitate the
+  //animation for going right, it's for going left
 
   #resetSliderPositionInterval(directionMethodString, numberOfIntervals) {
     let directionMethod;
@@ -207,6 +225,12 @@ export class ImageSlider {
     }
   }
 
+  //utilizes the moveSliderRight and moveSliderLeft methods in order to essentially cycle through the entire
+  //image slider back to the starting point, that being image 1. It will find the shortest distance there,
+  //and then use that to calculate the amount of intervals to execute the corresponding move right or move left method
+  //to reach the starting point as efficiently as possible. This method also restarts the automatic image cycling if
+  //it were otherwise disabled because the user clicked on one of the other buttons to interact with the slider
+
   #sliderButtonLogic(event) {
     const {
       sliderButtonLeftElement,
@@ -240,6 +264,7 @@ export class ImageSlider {
         return;
     }
   }
+  //logic behind each of the 3 buttons that exist for the image slider, either to move right, move left, or reset the slider
 
   #removeAllEventListeners() {
     this.#DOMcache.sliderButtonsContainerElement.removeEventListener(
@@ -250,6 +275,8 @@ export class ImageSlider {
     );
   }
 
+  //removes all event listeners being used in this module
+
   #addAllEventListeners() {
     this.#DOMcache.sliderButtonsContainerElement.addEventListener(
       "click",
@@ -259,10 +286,14 @@ export class ImageSlider {
     );
   }
 
+  //adds all event listeners where necessary in this module
+
   initializeEventListeners() {
     this.#removeAllEventListeners();
     this.#addAllEventListeners();
   }
+
+  //initializes the event listeners in a way to prevent extra event listeners
 
   #buildCompleteSlider() {
     this.#buildContainer();
@@ -281,6 +312,12 @@ export class ImageSlider {
     return sliderContainerElement;
   }
 
+  //responsible for facilitating the construction of the image slider, which calls
+  //various methods to build their own parts and add them to the DOM cache for later referencing in other parts of the class.
+  //These references are used to append each piece in the correct spot, which the output of this method
+  //is the entire constructed image slider but without the actual image elements, these image elements are 
+  //constructed and appended after this method is invoked naturally
+
   #buildContainer() {
     const SCrange = document.createRange(),
       sliderContainerFrag = SCrange.createContextualFragment(
@@ -295,6 +332,9 @@ export class ImageSlider {
     );
   }
 
+  //builds the main container of the image slider, which houses the display and the buttons containers
+  //The DOM ref for this container is stored in the DOM cache
+
   #buildDisplay() {
     const SDrange = document.createRange(),
       sliderDisplayFrag = SDrange.createContextualFragment(
@@ -308,6 +348,9 @@ export class ImageSlider {
       this.#stateData.uniqueIdentifier
     );
   }
+  //builds the display container for the image slider, doesn't come with the image elements already inside
+  //these are added afterwards utilizing the DOM reference to this element after being stored in the DOM cache in this
+  //instance
 
   #buildButtons() {
     const SBrange = document.createRange(),
@@ -344,6 +387,8 @@ export class ImageSlider {
     });
   }
 
+  //builds the buttons container as well as the buttons and stored a reference for each element in the DOM cache
+
   #buildImage() {
     const SIrange = document.createRange(),
       sliderImageFrag = SIrange.createContextualFragment(
@@ -355,6 +400,10 @@ export class ImageSlider {
 
     return sliderImageElement;
   }
+
+  //generic constructor for the display images, which will build one image on a single method call, but is
+  //used in conjuction with the addImageElements the construct the correct amount of images that you want and
+  //then append them to the display container
 
   #mapImageIdentifiers(imageSet) {
     imageSet.forEach((imageElement, index = 0) => {
@@ -370,6 +419,11 @@ export class ImageSlider {
       this.#stateData.DOMSliderImageClasses.push(imageIdentifier);
     });
   }
+
+  //takes an array of the image elements created and assigns some unique identifiers and default selected values
+  //so that the image elements are easy to reference. Each image element has some sort of unique identifier in the
+  //pattern of 'Image-#'. Also their assigned image identifier is added to the DOMSliderImageClasses which is used
+  //to facilitate the interactive logic of the image slider
 
   #addImageElements(numOfImages) {
     const createdImageElements = [];
@@ -390,9 +444,15 @@ export class ImageSlider {
     }
   }
 
+  //creates image elements based on the number of image elements defined by the instance of the class
+  //Sends the image elements off to be mapped and identified in the class state as well as the DOM
+  //then all of the image elements are appended to the display container afterwards
+
   #appendSlider(parentElement, assembledSlider) {
     parentElement.append(assembledSlider);
   }
+
+  //used to append the completely constructed and initialized image slider to a target element
 
   init(parentElement) {
     if (
@@ -412,4 +472,8 @@ export class ImageSlider {
       this.#stateData.init = !this.#stateData.init;
     }
   }
+
+  //the only API of this class, which allows you to create and append the customized image slider to a target
+  //element, which this method can only be invoked once per class instance in order to avoid potential conflicts
+  //in regards to element references
 }
