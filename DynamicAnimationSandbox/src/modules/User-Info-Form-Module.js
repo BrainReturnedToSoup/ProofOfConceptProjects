742aca9364eab9d91f3f081f11da00423e1e6655
NikkolasMinton <minton.nikkolas@gmail.com>
2023-06-14 03:32:19 -0500

    moved the slider module to the modules folder and now currently creating the foundation to create a reusable user form module

---
+++ DynamicAnimationSandbox/src/modules/User-Info-Form-Module.js
@@ -0,0 +1,41 @@
+export function UserInfoFormModule() {
+  const uniqueInstances = [];
+
+  class FragmentBuilder {
+    #templates = {
+      mainContainer: `
+
+        `,
+      email: `
+
+        `,
+      country: `
+
+        `,
+      zipCode: `
+
+        `,
+      password: `
+
+        `,
+      confirmPassword: `
+
+        `,
+    };
+  }
+
+  class ElementCacheManager {
+    #refsCache = {
+        
+    }
+  }
+
+  class FunctionalityInitializer {}
+
+  class UserInfoForm {}
+  function argSanitizer() {}
+
+  function newClassInstance() {}
+
+  return { newClassInstance };
+}
