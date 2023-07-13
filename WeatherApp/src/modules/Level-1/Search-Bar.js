65f0ef017c76415489de0f1b0bd77dc5f77d60a4
NikkolasMinton <minton.nikkolas@gmail.com>
2023-07-13 06:09:17 -0500

    the search bar fragment constructor class is finished, going to move onto the dynamic options rendering class

--- WeatherApp/src/modules/Level-1/Search-Bar.js
+++ WeatherApp/src/modules/Level-1/Search-Bar.js
@@ -464,19 +464,179 @@ class ElementRefManager {
 }
 
 class DynamicOptionsManager {
-    constructor()
+  constructor()
 
-    activate() {}
+  requestOptionsToRender(currentInput) {}
 
-    deactivate() {}
+  activate() {}
+
+  deactivate() {}
 }
 
 class SearchBarConstructor {
-  constructor() {}
+  constructor(elementRefManager, uniqueIdentifier, action = "#", method = "post", addDataList = false) {
+    try {
+      //validate reference manager
+      if(elementRefManager instanceof ElementRefManager) {
+        this.#configData.elementRefManager = elementRefManager
+      } else {
+        throw new ReferenceError(`Failed to create new '${this.constructor.name}' class instance, the supplied element reference manager is not an instance of the 'ElementRefManager' class, received '${elementRefManager}'`)
+      }
+      
+      //returing the validity of arguments that are supposed to be a string
+      const isUniqueIdentifierValid = this.#isStringValid(uniqueIdentifier),
+      isActionValid = this.#isStringValid(action),
+      isMethodValid = this.#isStringValid(method);
 
-  #formControlElementTemplates = {};
+      if(isUniqueIdentifierValid) {
+        this.#configData.uniqueIdentifier = uniqueIdentifier;
+      } else {
+        throw new ReferenceError(`Failed to create new '${this.constructor.name}' class instance, the supplied 'unique identifier' failed validation, must be a non empty string without any spaces within it, received '${uniqueIdentifier}' as the supplied unique identifier`)
+      }
 
-  buildCompleteSearchBar() {}
+      if(isActionValid) {
+        this.#configData.action = action;
+      } else {
+        throw new ReferenceError(`Failed to create new '${this.constructor.name}' class instance, the supplied 'action' failed validation, must be a non empty string without any spaces within it, received '${action}' as the supplied action`)
+      }
+
+      if(isMethodValid) {
+        this.#configData.method = method;
+      } else {
+        throw new ReferenceError(`Failed to create new '${this.constructor.name}' class instance, the supplied 'method' failed validation, must be a non empty string without any spaces within it, received '${method}' as the supplied method`)
+      }
+
+      //check if the value supplied for the addDataList argument is a boolean to determine whether to use a data list in the construction
+      if(typeof addDataList === "boolean") {
+
+      } else {
+        throw new TypeError(`Failed to create new '${this.constructor.name}' class instance, the supplied value for the 'addDataList' parameter is not a boolean, received '${addDataList}' with a data type of '${typeof addDataList}'`)
+      }
+
+    } catch (error) {
+      console.error(error, error.stack)
+    } 
+  }
+  #isStringValid(string) {
+    if(string === "") {
+      return false;
+    } //make sure it's not an empty string
+
+    if(string.replace(/\s/, "") !== string) {
+      return false;
+    } //make sure the string does not contain any spaces whatsoever
+
+    return true;
+  }
+
+  //holds data that will configure the behavior and the structure of
+  //the produced search bar
+  #configData = {
+    action: "#",
+    method: "post",
+    addDataList: false,
+    uniqueIdentifier: "null",
+  }
+
+  //holds references to helper classes which will help with some sort
+  //of internal or bigger picture functionality
+  #helperClasses = {
+    elementRefManager: null
+  }
+
+  //used to construct the various elements on command, and the corresponding
+  //elements will match what the config defines
+  #elementConstructors = {
+    form: () => {
+      const formElement = document.createElement("form"),
+      generalClass = "search-bar";
+
+      formElement.classList.add(generalClass);
+      formElement.classList.add(this.#configData.uniqueIdentifier); //add both of the necessary classes, one a general tag, the other the unique identifier
+
+      formElement.setAttribute("action", this.#configData.action); //define both the action and method of the form, which will determine the behavior of
+      formElement.setAttribute("method", this.#configData.method); //the constructed search bar 
+
+      return formElement;
+    },
+    searchBarInput: () => {
+      const searchBarInput = document.createElement("input"),
+      generalClass = "search-bar-input";
+
+      searchBarInput.classList.add(generalClass); 
+      searchBarInput.classList.add(this.#configData.uniqueIdentifier);  //add both of the necessary classes, one a general tag, the other the unique identifier
+      
+      searchBarInput.setAttribute("type", "text");  //define mandatory attributes such as the type, and the name of the input 
+      searchBarInput.setAttribute("name", `search-bar-input_${this.#configData}_var`) //This way the search bar instance can be both semantic and unique in nature
+
+      if(this.#configData.addDataList) {
+        searchBarInput.setAttribute("list", `search-bar-input-options-${this.#configData.uniqueIdentifier}`); //add an attribute based on whether a data list will be added to this search bar
+      }
+
+      return searchBarInput;
+    },
+    dataList: () => {
+        const dataList = document.createElement("datalist"),
+        generalClass = "search-bar-input-datalist";
+
+        dataList.classList.add(generalClass);
+        dataList.classList.add(this.#configData.uniqueIdentifier); //add both of the necessary classes, one a general tag, the other the unique identifier
+
+        dataList.setAttribute("id", `search-bar-input-options-${this.#configData.uniqueIdentifier}`) //define the mandatory attribute that will link the corresponding data list to its input
+
+        return dataList;
+      
+    }
+  }
+  
+
+  //will be used to choreograph the assembly of the entire
+  //search bar, as well as store each individual element created into the 
+  //reference manager
+  #constructSearchBar() {
+    const formElement = this.#elementConstructors.form(),
+    searchBarInputElement = this.#elementConstructors.searchBarInput(),
+    formElementClassList = Array.from(formElement.classList),
+    searchBarInputElementClassList = Array.from(searchBarInputElement.classList);
+
+    this.#storeElementRefs(formElementClassList[0], formElement); //store both the form element and the input element in the reference manager and use their general class assigned to
+    this.#storeElementRefs(searchBarInputElementClassList[0], searchBarInputElement); //them as their unique identifiers within storage
+
+    if(this.#configData.addDataList) { //condition to also add a data list if the configuration designates such
+      const dataListElement = this.#elementConstructors.dataList(), //will store said element in the same way as the previous two
+      dataListElementClassList = Array.from(dataListElement);
+
+      this.#storeElementRefs(dataListElementClassList[0], dataListElement);
+
+      formElement.append(searchBarInputElement); //this is where the appending will happen, thus ensuring a correct configuration 
+      formElement.append(dataListElement); //in the node tree
+
+      return formElement;
+    } else {
+      formElement.append(searchBarInputElement); //same thing, but just without the addition of the data list
+
+      return formElement
+    }
+
+    //return the form element at the end, where it will be completely configured, and all of its references were correctly stored in the element reference manager
+  }
+
+  //an easier to understand means of interacting with the helper class element reference manager
+  #storeElementRefs(key, value) {
+    this.#helperClasses.elementRefManager.addRef(key, value)
+  }
+
+  //the api to retrieve a constructed search bar after the class instance was created
+  //the functionality hasn't been appended in this stage. the api is simple as it simply acts as 
+  //the entry point into the class, and holds the try catch block to catch any errors
+  buildSearchBar() {
+    try {
+      const completeSearchBar = this.#constructSearchBar();
+      return completeSearchBar;
+    } catch (error) {
+      console.error(error, error.stack)
+    }
+  }
 }
 
 export class SearchBar {
@@ -488,5 +648,5 @@ export class SearchBar {
 
   #searchBarAppended = false;
 
-  appendSearchBar() {}
+  appendSearchBar(parentElement) {}
 }
