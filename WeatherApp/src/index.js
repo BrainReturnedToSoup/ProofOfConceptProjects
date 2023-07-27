6cf0dec5be826d9db8ca75cee92b9f4cf8811779
NikkolasMinton <minton.nikkolas@gmail.com>
2023-07-27 00:06:40 -0500

    structured the weather data manager class, now the functionality to implement is mainly for its helper classes

--- WeatherApp/src/index.js
+++ WeatherApp/src/index.js
@@ -1,13 +1,1 @@
-import { ElementRefManager } from "./modules/Level-0/Element-Ref-Manager";
-import { WeatherLocationSearchBar } from "./modules/Level-2/Weather-Location-Search-Bar";
 
-const searchBar = new WeatherLocationSearchBar("test", "null");
-
-searchBar.append(document.body);
-
-searchBar.subscribeToSearchBarInput("test", (input) => {
-  console.log("inputValue", input);
-});
-searchBar.subscribeToApiData("test", (data) => {
-  console.log("apiData", data);
-});
